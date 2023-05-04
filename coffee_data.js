78427f0879a38a8f25af0900b9e407279f9065de
Davonne Vigil <davonnejv@gmail.com>
2023-05-04 04:27:54 -0500

    adding coffee_data.js and index.js

---
+++ coffee_data.js
@@ -0,0 +1,85 @@
+//Block 17 Workshop: Coffee Menu
+
+
+// Prompt 1:
+// Clean the coffee_data file:
+
+// Checklist:
+// a. All prices should be numbers.
+// b. All items should be strings.
+// c. Objects and properties should have commas seperating them.
+// d. Connect the coffee_data.js file to the index.js file.
+
+const coffeeMenu = [
+    {
+        name: "cappuccino",
+        price: 8,
+        seasonal: false
+    },
+    {
+        name: "espresso",
+        price: 5,
+        seasonal: false
+    },
+    {
+        name: "latte",
+        price: 5,
+        seasonal: false
+    },
+    {
+        name: "affogato",
+        price: 9,
+        seasonal: true
+    }, 
+    {
+        name: "macchiato",
+        price: 6,
+        seasonal: false
+    },
+    {
+        name: "americano",
+        price: 7,
+        seasonal: false
+    },
+    {
+        name: "iced coffee",
+        price: 6,
+        seasonal: false
+    },
+    {
+        name: "frappe",
+        price: 8,
+        seasonal: false
+    },
+    {
+        name: "cuban espresso",
+        price: 10,
+        seasonal: true
+    }
+];
+
+//Connect the coffee_data.js file to the index.js file.
+module.exports = coffeeMenu
+
+//Console.table Coffee Menu to check
+console.table(coffeeMenu);
+
+//Print an array of all the drinks on the menu
+const allDrinks = coffeeMenu.map(drink => drink.name);
+console.log(allDrinks);
+
+//Print an array of drinks that cost 5 and under
+const cheaperDrinks = coffeeMenu.filter(drink => drink.price <= 5);
+const drinkNameAndPrices = cheaperDrinks.map(drink => {
+    return { name: drink.name, price: drink.price };
+});
+
+console.log(drinkNameAndPrices);
+
+//Print an array of drinks that are priced at an even number
+const evenPricedDrinks = coffeeMenu.filter(drink => drink.price % 2 === 0);
+console.log(evenPricedDrinks);
+
+
+
+
---
+++ index.js
@@ -0,0 +1,4 @@
+//Connecting coffee_data.js to this file index.js
+
+const coffeeMenu = require("./coffee_data.js");
+console.log(coffeeMenu);
