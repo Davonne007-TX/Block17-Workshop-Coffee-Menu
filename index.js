bec616399be5f949c5581dde3241e671a0314ab7
Davonne Vigil <davonnejv@gmail.com>
2023-05-04 14:05:41 -0500

    making code on index.js

--- index.js
+++ index.js
@@ -2,3 +2,46 @@
 
 const coffeeMenu = require("./coffee_data.js");
 console.log(coffeeMenu);
+
+//Console.table Coffee Menu to check
+console.table(coffeeMenu);
+
+//Print an array of all the drinks on the menu
+const allDrinks = coffeeMenu.map(drink => drink.name);
+console.log(allDrinks);
+
+
+//Print an array of drinks that cost 5 and under
+const cheaperDrinks = coffeeMenu.filter(drink => drink.price <= 5);
+const drinkNameAndPrices = cheaperDrinks.map(drink => {
+    return { name: drink.name, price: drink.price };
+});
+
+console.log(drinkNameAndPrices);
+
+
+//Print an array of drinks that are priced at an even number
+const evenPricedDrinks = coffeeMenu.filter(drink => drink.price % 2 === 0);
+console.log(evenPricedDrinks);
+
+
+//Print the total if you were to order one of every drink
+const totalPrice = coffeeMenu.reduce((total, drink) => {
+    return total + drink.price;
+}, 0);
+
+console.log(totalPrice);
+
+
+// Print an array with all the drinks that are seasonal
+const seasonalDrinks = coffeeMenu.filter(drink => drink.seasonal).map(drink => drink.name);
+console.log(seasonalDrinks);
+
+
+//Print all the seasonal drinks with the words "with imported beans" after the item name
+  //For example: "affogato with imported beans"
+const importedBeans = coffeeMenu.filter(drink => drink.seasonal);
+
+importedBeans.forEach(drink => {
+    console.log(drink.name + " with imported beans!");
+});
