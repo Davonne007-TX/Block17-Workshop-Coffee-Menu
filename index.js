a958cd80b8f8d9640a1032cdb22307f7d5d6d38f
Davonne Vigil <davonnejv@gmail.com>
2023-05-04 19:48:44 -0500

    putting console logs back, for peer reviewer

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ console.table(coffeeMenu);
 
 //Print an array of all the drinks on the menu
 const allDrinks = coffeeMenu.map(drink => drink.name);
+console.log(allDrinks);
 
 
 //Print an array of drinks that cost 5 and under
@@ -15,20 +16,23 @@ const cheaperDrinks = coffeeMenu.filter(drink => drink.price <= 5);
 const drinkNameAndPrices = cheaperDrinks.map(drink => {
     return { name: drink.name, price: drink.price };
 });
-
+console.log(drinkNameAndPrices);
 
 //Print an array of drinks that are priced at an even number
 const evenPricedDrinks = coffeeMenu.filter(drink => drink.price % 2 === 0);
+console.log(evenPricedDrinks);
 
 
 //Print the total if you were to order one of every drink
 const totalPrice = coffeeMenu.reduce((total, drink) => {
     return total + drink.price;
 }, 0);
+console.log(totalPrice);
 
 
 // Print an array with all the drinks that are seasonal
 const seasonalDrinks = coffeeMenu.filter(drink => drink.seasonal).map(drink => drink.name);
+console.log(seasonalDrinks);
 
 
 //Print all the seasonal drinks with the words "with imported beans" after the item name
